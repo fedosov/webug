c85a5825819962e1322c4f7898a0a179ec60f598
Mikhail Fedosov <tbs.micle@gmail.com>
2013-05-29 21:50:09 +0400

    log syntax highlighting (by https://github.com/isagalaev/highlight.js)

--- webug/assets/javascripts/panel.js
+++ webug/assets/javascripts/panel.js
@@ -6,9 +6,6 @@
 
 function log(item)
 {
-	var entries = $("#entries");
-	var entry = $("<li class='entries__item'></li>").addClass("entry_type__" + item[0]["Type"]);
-
 	/*
 	LOG: "log",
 	DEBUG: "debug",
@@ -20,7 +17,12 @@ function log(item)
 	GROUP_END: "groupEnd"
 	*/
 
-	entry.append($("<div class='entry__message'></div>").text(item[1]));
+	var entries = $("#entries");
+	var entry = $("<li class='entries__item'></li>").addClass("entry_type__" + item[0]["Type"]);
+
+	var entry__message = $("<pre class='entry__message'></pre>").text(item[1]);
+	hljs.highlightBlock(entry__message[0]);
+	entry.append(entry__message);
 	entry.append($("<div class='entry__source'></div>").html(item[0]["File"] + ":<strong>" + item[0]["Line"] + "</strong>"));
 
 	entries.append(entry);
