851bf0e22b2385c31dcbe0ce459fed2f75c6a801
GodLesZ <godlesz@hotmail.de>
2015-06-10 09:32:27 +0200

    No more empty lines
    Fixed empty output if FirePHP failed to detect file and line

--- webug/assets/javascripts/logging.js
+++ webug/assets/javascripts/logging.js
@@ -3,9 +3,12 @@ chrome.runtime.onMessage.addListener(function(message, sender)
 	if (message.type === "webug.log")
 	{
 		var args = [];
-		if (["group", "groupEnd"].indexOf(message.log_type) === -1)
+		if (
+			["group", "groupEnd"].indexOf(message.log_type) === -1 && 
+			message.meta.File.length > 0
+		)
 		{
-			console.log("%c" + message.meta.File + ":" + message.meta.Line, "color: #aaa;")
+			console.log("%c" + message.meta.File + ":" + (message.meta.Line :: '<unknown>'), "color: #aaa;")
 		}
 		if (message.log_type === "table")
 		{
